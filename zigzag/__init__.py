6da698af7e6c81039a50459049c70aa63581c722
Alex Glebov <alex.glebov@gmail.com>
2025-11-30 15:47:53 +1100

    0.3.5 implement compute_performance
    Extending exiting 'compute_segment_returns' and 'max_drawdown'  function
    by function 'compute_performance' calculated max drawdown before first
    PEAK , max gain at this PEAK  and distance to this PEAK for each element
    in array X.
            On raising trend values for max drawdown is zero.
    For last element of array X values always zero as this is last
    point an should be excluded
    technically arrays should be one elements shorter the X and pivot arrays
    
    modify test_core to match past changes in core.py where
    this code
    
    -#    up_thresh += 1
    -#    down_thresh += 1
    
    were moved to the top level function 'peak_valley_pivots_detailed' from
    inner function identify_initial_pivot as it used in both functions
    
    also added basic tests for 'compute_performance'

--- zigzag/__init__.py
+++ zigzag/__init__.py
@@ -1,4 +1,19 @@
-from zigzag.core import *
+#from zigzag.core import *
+from numpy import double
+from typing import NamedTuple
 
 PEAK = 1
 VALLEY = -1
+SIDEMOVE = 2 # future development 
+
+__all__ = [
+  'PEAK',
+  'VALLEY',
+  'identify_initial_pivot',
+  'max_drawdown',
+  'peak_valley_pivots',
+  'pivots_to_modes',
+  'compute_segment_returns',
+  'compute_performance',
+  'compute_performance_nd',
+  ]
